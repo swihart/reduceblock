ae52df6a569452258184cb1a7be905b642d3152d
swihart <bruce.swihart@gmail.com>
2015-04-16 08:49:00 -0400

    add one.m.ratio

---
+++ R/one.minus.ratio.100.R
@@ -0,0 +1,13 @@
+#' Helper function for TRA and TBA
+#'
+#' TRA and TBA both have the same form: 100*(1 - num/den).
+#' Just specify num and den and get the desired calculation.
+#' @param num the numerator in 100*(1 - num/den)
+#' @param den the numerator in 100*(1 - num/den)
+#' @keywords tra tba pip pim
+#' @export
+#' @examples
+#' one.minus.ratio.100(prevalence(rho_in=2.7, muci_in=10, theta_in=1.75),
+#'                     prevalence(muci_in=10, theta_in=1.75))
+
+one.minus.ratio.100 <- function(num,den){100*(1-num/den)}
